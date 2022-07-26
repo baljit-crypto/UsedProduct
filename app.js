fc7cfa906f1ae38327bacfa8ffce32d16039a546
baljit-crypto <baljitsingh4068@gmail.com>
2022-07-26 19:14:19 -0400

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
+require('./app_api/models/db')
+var productRouter = require('./app_api/routes/products')
+app.use('/api',productRouter)
+
 
 app.listen(port,() =>{
         console.log(`listening to port ${port}`)
