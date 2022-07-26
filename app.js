765037ff365f98ae97bef057014dd45c786d7fe9
baljit-crypto <baljitsingh4068@gmail.com>
2022-07-26 18:49:40 -0400

    make it better

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require("express")
 var app = express()
-var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+var server_port = process.env.PORT || 5000;
 
 
 app.use('/',function(){
 })
 
 
-app.listen(server_port,server_host,() =>{
+app.listen(server_port,() =>{
         console.log(`listening to port ${port}`)
 })
 
